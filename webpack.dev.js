c57c3c1f4a95cd6221c056fc4addef6f02588d34
scinth <rodnielbriones@gmail.com>
2022-11-03 13:49:22 +0800

    webpack development build config

---
+++ webpack.dev.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SOURCE = path.resolve(__dirname, 'src');
+
+module.exports = {
+	mode: 'development',
+	output: {
+		filename: '[contenthash].js',
+	},
+	devtool: false,
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: SOURCE,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						plugins: ['@babel/plugin-transform-runtime'],
+						cacheDirectory: true,
+					},
+				},
+			},
+			{
+				test: /\.html$/,
+				use: 'html-loader',
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|pdf)$/i,
+				include: SOURCE,
+				type: 'asset/resource',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			favicon: './src/favicon.ico',
+		}),
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'src'),
+		},
+		compress: true,
+		port: 7777,
+	},
+};
